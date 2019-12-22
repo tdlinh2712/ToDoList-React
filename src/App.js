5f8bb4406f1a9410888f2ce58153b6a98584eaf4
Linh <linh@Linhs-MacBook-Air-2.local>
2019-12-22 10:21:51 +0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Note from './Note/Note';
+import NoteForm from './NoteForm/NoteForm';
+import {DB_CONFIG} from './config/config';
+import firebase from 'firebase/app';
+import 'firebase/database';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    //set up react state of our Component
+    this.addNote = this.addNote.bind(this);
+    this.app = firebase.initializeApp(DB_CONFIG);
+    this.database=this.app.database().ref().child('notes');
+    this.state = {
+      notes: [],
+    }
+  }
+
+  componentWillMount() {
+    const previousNotes = this.state.notes;
+    //data snapshot
+    this.database.on('child_added',snap =>{
+      previousNotes.push({
+        id:snap.key,
+        noteContent:snap.val().noteContent,
+      })
+
+      this.setState({
+        notes:previousNotes,
+
+      })
+    })
+  }
+
+  addNote(note) {
+    this.database.push().set({
+      noteContent:note
+    })
+  }
+  render() {
+    return (
+      <div className="notesWrapper">
+        <div className="notesHeader">
+          <div className="Heading"> React & Firebase To-do List</div>
+        </div>
+        <div className="notesBody">
+          {
+            this.state.notes.map((note)=>{
+              return(
+              <Note noteContent={note.noteContent} noteId={note.id} key={note.id}/>
+              )
+            })
+          }
+        </div>
+        <div className="notesFooter">
+          <NoteForm addNote={this.addNote}/>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/config/config.js
@@ -0,0 +1,13 @@
+
+  // Your web app's Firebase configuration
+  export const DB_CONFIG = {
+    apiKey: "AIzaSyBr0OZ1RNjFNq8V_sNCKhT0PCbzBKPaDRw",
+    authDomain: "react-notes-190b1.firebaseapp.com",
+    databaseURL: "https://react-notes-190b1.firebaseio.com",
+    projectId: "react-notes-190b1",
+    storageBucket: "react-notes-190b1.appspot.com",
+    messagingSenderId: "709339443663",
+    appId: "1:709339443663:web:75f3c1f98bb0cf2742e2ba",
+    measurementId: "G-Q2F2WBXVLQ"
+  };
+  // Initialize Firebase
